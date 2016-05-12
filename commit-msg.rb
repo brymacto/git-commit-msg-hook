1dcf00ef7d56f56473790d4499a86fdb598b6d8f
Bryan MacDiarmid <bryanm@nulogy.com>
2016-05-11 21:08:45 -0400

    Sets up skeleton for testing and implementation

--- commit-msg.rb
+++ commit-msg.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
 
 def run
-  return commit_message
+  if commit_message == 'hi'
+    exit 0 # Commit
+  end
+  exit 1 # Abort
 end
 
 def commit_message
   File.open(ARGV[0]).read
 end
 
-run
+def branch_name
+  `git branch | grep "^\*"`[2..-1]
+end
+
+# run
 
 
--- spec/commit-msg-spec.rb
+++ spec/commit-msg-spec.rb
@@ -1,14 +1,41 @@
 require_relative '../commit-msg'
 
 describe "commit message script" do
-  it "Works" do
-    opened_file = instance_double("File")
+  it "Aborts if commit message does not contain branch number" do
     test_commit_message = "Test commit message"
+    test_branch_name = "9999_"
+    opened_file = instance_double("File")
     allow(File).to receive(:open).and_return(opened_file)
+    allow(self).to receive(:branch_name).and_return(test_branch_name)
     allow(opened_file).to receive(:read) { test_commit_message }
 
-    result = run
+    begin
+      run
+    rescue SystemExit=>e
+      assert_exit_aborts_commit(e)
+    end
+  end
 
-    expect(result).to eq("Test commit message")
+  it "Does not abort if commit message contains branch number" do
+    test_commit_message = "#9999 - Test commit message"
+    test_branch_name = "9999_"
+    opened_file = instance_double("File")
+    allow(File).to receive(:open).and_return(opened_file)
+    allow(self).to receive(:branch_name).and_return(test_branch_name)
+    allow(opened_file).to receive(:read) { test_commit_message }
+
+    begin
+      run
+    rescue SystemExit=>e
+      assert_exit_does_not_abort_commit(e)
+    end
+  end
+
+  def assert_exit_aborts_commit(e)
+    expect(e.status).to eq(1)
+  end
+
+  def assert_exit_does_not_abort_commit(e)
+    expect(e.status).to eq(0)
   end
 end
