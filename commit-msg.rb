93582a01219aa221361ae780fd3d6aa1c2265320
Bryan MacDiarmid <bryanm@nulogy.com>
2016-05-11 21:25:11 -0400

    Completes implementation

--- commit-msg.rb
+++ commit-msg.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
 
 def run
-  if commit_message == 'hi'
-    exit 0 # Commit
-  end
-  exit 1 # Abort
+  abort if ticket_number_from_branch_name && !commit_message_starts_with_ticket_number
+  commit
+end
+
+def commit_message_starts_with_ticket_number
+  commit_message.start_with?("##{ticket_number_from_branch_name}")
+end
+
+def ticket_number_from_branch_name
+  /^\d{4,}/.match(branch_name)
 end
 
 def commit_message
@@ -13,6 +19,15 @@ end
 
 def branch_name
   `git branch | grep "^\*"`[2..-1]
+end
+
+def commit
+  exit 0
+end
+
+def abort
+  puts "Aborting commit - commit message must start with the number of your working branch (##{ticket_number_from_branch_name})."
+  exit 1
 end
 
 # run
--- spec/commit-msg-spec.rb
+++ spec/commit-msg-spec.rb
@@ -1,9 +1,9 @@
 require_relative '../commit-msg'
 
 describe "commit message script" do
-  it "Aborts if commit message does not contain branch number" do
+  it "Aborts if branch name starts with branch number, but commit message does not" do
+    test_branch_name = "9999_my_branch"
     test_commit_message = "Test commit message"
-    test_branch_name = "9999_"
     opened_file = instance_double("File")
     allow(File).to receive(:open).and_return(opened_file)
     allow(self).to receive(:branch_name).and_return(test_branch_name)
@@ -16,9 +16,40 @@ describe "commit message script" do
     end
   end
 
+  it "Aborts if branch numbers do not match" do
+    test_branch_name = "9999_my_branch"
+    test_commit_message = "#1111 - Test commit message"
+    opened_file = instance_double("File")
+    allow(File).to receive(:open).and_return(opened_file)
+    allow(self).to receive(:branch_name).and_return(test_branch_name)
+    allow(opened_file).to receive(:read) { test_commit_message }
+
+    begin
+      run
+    rescue SystemExit=>e
+      assert_exit_aborts_commit(e)
+    end
+  end
+
+
   it "Does not abort if commit message contains branch number" do
+    test_branch_name = "9999_my_branch"
     test_commit_message = "#9999 - Test commit message"
-    test_branch_name = "9999_"
+    opened_file = instance_double("File")
+    allow(File).to receive(:open).and_return(opened_file)
+    allow(self).to receive(:branch_name).and_return(test_branch_name)
+    allow(opened_file).to receive(:read) { test_commit_message }
+
+    begin
+      run
+    rescue SystemExit=>e
+      assert_exit_does_not_abort_commit(e)
+    end
+  end
+
+  it "Does not abort if branch name does not include branch number (regardless of whether commit message includes number)" do
+    test_commit_message = "my message"
+    test_branch_name = "master"
     opened_file = instance_double("File")
     allow(File).to receive(:open).and_return(opened_file)
     allow(self).to receive(:branch_name).and_return(test_branch_name)
